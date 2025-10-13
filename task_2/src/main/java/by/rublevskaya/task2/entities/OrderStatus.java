dc282c17f2418747635e07e4b3b1a4f7a4fc607f
graffegiraffe <katerublevsk@icluod.com>
2025-10-13 21:14:36 +0300

    Remove task_1 and task_2 from tasks-task3 branch

--- task_2/src/main/java/by/rublevskaya/task2/entities/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package by.rublevskaya.task2.entities;
-
-public enum OrderStatus {
-    NEW,
-    PROCESSING,
-    SHIPPED,
-    DELIVERED,
-    CANCELLED
-}
--- task_2/src/main/java/by/rublevskaya/task2/metrics/MetricCalculator.java
+++
@@ -1,9 +0,0 @@
-package by.rublevskaya.task2.metrics;
-
-import by.rublevskaya.task2.entities.Order;
-
-import java.util.List;
-
-public interface MetricCalculator<T> {
-    T calculate(List<Order> orders);
-}
