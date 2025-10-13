403aa2183e862e5c1204c3554f6fca43a1875449
graffegiraffe <katerublevsk@icluod.com>
2025-10-13 21:10:00 +0300

    Full implementation of task 3, namely there are two factions: World and Wednesday and neutral object - Factory. Each faction is trying to create an army of robots, but to do so they need parts for the robots.Robot parts are divided into: head, torso, hand, feet. To build one robot you need 1 head, 1 torso, 2 hands, 2 feet. They are produced by a neutral Factory, which produces no more than 10 parts every day. The type of parts is chosen randomly. At night, the factions go to the Factory to get parts for the robots (each faction can carry no more than 5 parts). The factions and the factory each work in their own thread. Determine who will have the strongest army after 100 days.

---
+++ task_3/src/main/java/by/rublevskaya/task3/factions/WednesdayFaction.java
@@ -0,0 +1,16 @@
+package by.rublevskaya.task3.factions;
+
+import by.rublevskaya.task3.assembler.RobotAssembler;
+import by.rublevskaya.task3.factory.ResourceStorage;
+
+public class WednesdayFaction extends Faction {
+
+    public WednesdayFaction(ResourceStorage factoryStorage) {
+        super(factoryStorage);
+    }
+
+    @Override
+    protected int assembleRobots() {
+        return RobotAssembler.assembleRobots(getInventory());
+    }
+}
