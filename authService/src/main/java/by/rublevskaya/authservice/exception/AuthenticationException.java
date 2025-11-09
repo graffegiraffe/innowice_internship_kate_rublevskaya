9a8922ca9826186aacc40fcc16d5f255664e0d54
graffegiraffe <katerublevsk@icluod.com>
2025-11-10 00:39:37 +0300

    create custom exception - Create AuthenticationException for auth failures - Create TokenException for JWT token errors - Create UserAlreadyExistsException for registration conflicts

---
+++ authService/src/main/java/by/rublevskaya/authservice/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package by.rublevskaya.authservice.exception;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
