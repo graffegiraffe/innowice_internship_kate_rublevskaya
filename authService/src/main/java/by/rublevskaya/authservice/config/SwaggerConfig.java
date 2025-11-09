5cde5f37db9b533791dba4aa8837f98bc7532450
graffegiraffe <katerublevsk@icluod.com>
2025-11-10 00:15:27 +0300

    add Swagger documentation configuration

---
+++ authService/src/main/java/by/rublevskaya/authservice/config/SwaggerConfig.java
@@ -0,0 +1,58 @@
+package by.rublevskaya.authservice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+public class SwaggerConfig {
+    @Value("${springdoc.openapi.dev-url:http://localhost:8081}")
+    private String devUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("katusha@example.com");
+        contact.setName("Katusha");
+
+        License license = new License()
+                .name("MIT License")
+                .url("https://choosealicense.com/licenses/mit/");
+
+        Info info = new Info()
+                .title("Auth Service API")
+                .version("1.0")
+                .contact(contact)
+                .description("Authentication and Authorization Service with JWT")
+                .license(license);
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Authentication");
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(devServer))
+                .addSecurityItem(securityRequirement)
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", securityScheme));
+    }
+}
