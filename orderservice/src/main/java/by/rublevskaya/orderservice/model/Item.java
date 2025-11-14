94456301f2a783a8450dc1fd5fcdf45056133e89
graffegiraffe <katerublevsk@icluod.com>
2025-11-14 22:10:11 +0300

    feat(orderservice): Add JPA entities and enum

---
+++ orderservice/src/main/java/by/rublevskaya/orderservice/model/Item.java
@@ -0,0 +1,28 @@
+package by.rublevskaya.orderservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "items")
+@Getter
+@Setter
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+}
