b0fafd208333403aa3f4d29d9148341bdfeb4f68
Kate Rublevskaya <145375460+graffegiraffe@users.noreply.github.com>
2026-01-10 01:51:36 +0300

    feat(Payment): Update config

--- paymentservice/src/main/java/by/rublevskaya/paymentservice/config/AppConfig.java
+++ paymentservice/src/main/java/by/rublevskaya/paymentservice/config/AppConfig.java
@@ -2,8 +2,10 @@ package by.rublevskaya.paymentservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -13,7 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan("by.rublevskaya")
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
-    public HandlerInterceptor logInterceptor;
+    private final HandlerInterceptor logInterceptor;
 
     @Autowired
     public AppConfig(@Qualifier(value = "log-interceptor") HandlerInterceptor logInterceptor) {
@@ -23,5 +25,10 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(logInterceptor).addPathPatterns("/**");
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
