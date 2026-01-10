846227eab86627d6cdc226219dc4310b10276d1a
Kate Rublevskaya <145375460+graffegiraffe@users.noreply.github.com>
2026-01-10 22:43:31 +0300

    feat(order service): set Event and service setup

--- orderservice/src/main/java/by/rublevskaya/orderservice/service/OrderServiceImpl.java
+++ orderservice/src/main/java/by/rublevskaya/orderservice/service/OrderServiceImpl.java
@@ -4,7 +4,9 @@ import by.rublevskaya.orderservice.client.UserServiceClient;
 import by.rublevskaya.orderservice.dto.OrderRequestDto;
 import by.rublevskaya.orderservice.dto.OrderResponseDto;
 import by.rublevskaya.orderservice.dto.UserDto;
+import by.rublevskaya.orderservice.event.OrderEvent;
 import by.rublevskaya.orderservice.exception.ResourceNotFoundException;
+import by.rublevskaya.orderservice.kafka.OrderProducer;
 import by.rublevskaya.orderservice.mapper.OrderMapper;
 import by.rublevskaya.orderservice.model.Item;
 import by.rublevskaya.orderservice.model.Order;
@@ -16,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,6 +31,7 @@ public class OrderServiceImpl implements OrderService {
     private final ItemRepository itemRepository;
     private final OrderMapper orderMapper;
     private final UserServiceClient userServiceClient;
+    private final OrderProducer orderProducer;
 
     @Override
     @Transactional
@@ -38,6 +42,15 @@ public class OrderServiceImpl implements OrderService {
         addItemsToOrder(order, requestDto.getItems());
         Order savedOrder = orderRepository.save(order);
 
+        OrderEvent event = new OrderEvent(
+                savedOrder.getId(),
+                savedOrder.getUserId(),
+                savedOrder.getOrderItems().stream()
+                        .map(item -> item.getItem().getPrice().multiply(BigDecimal.valueOf(item.getQuantity())))
+                        .reduce(BigDecimal.ZERO, BigDecimal::add),
+                "PENDING"
+        );
+        orderProducer.sendOrderEvent(event);
         return enrichOrderResponseWithUser(savedOrder, bearerToken);
     }
 
